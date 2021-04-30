083dea0b53befbf5ac6300a166284ed6ea14503f
barrasso <4982406+barrasso@users.noreply.github.com>
2021-04-29 23:00:18 -0400

    Refactor widget and update design

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+import { ToTopOutlined } from "@ant-design/icons";
 
 const WidgetDiv = document.querySelector('.exfil_widget')
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App widget={WidgetDiv}/>
-  </React.StrictMode>,
-  WidgetDiv
-);
+class App extends React.Component {
+  state = {
+    isLoading: false
+  };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  setLoading() {
+    this.setState({ isLoading: true });
+
+    setTimeout(() => {
+      this.setState({ isLoading: false })
+    }, 6000);
+  };
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <>
+      <Button
+        type="primary"
+        icon={<ToTopOutlined />}
+        loading={isLoading}
+        onClick={() => this.setLoading()}
+      >
+        Exfil Now
+      </Button>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, WidgetDiv);
