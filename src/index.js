61e551667ed0a960262c61266169ce582c8c488f
barrasso <4982406+barrasso@users.noreply.github.com>
2021-04-30 00:04:34 -0400

    Add async modal

--- src/index.js
+++ src/index.js
@@ -2,39 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd/dist/antd.css';
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
 import { ToTopOutlined } from "@ant-design/icons";
 
 const WidgetDiv = document.querySelector('.exfil_widget')
 
-class App extends React.Component {
-  state = {
-    isLoading: false
+const App = () => {
+
+  const [visible, setVisible] = React.useState(false);
+  const [confirmLoading, setConfirmLoading] = React.useState(false);
+  const [modalText, setModalText] = React.useState('Content of the modal');
+
+  const showModal = () => {
+    setVisible(true);
   };
 
-  setLoading() {
-    this.setState({ isLoading: true });
-
+  const handleOk = () => {
+    setModalText('The modal will be closed after two seconds');
+    setConfirmLoading(true);
     setTimeout(() => {
-      this.setState({ isLoading: false })
-    }, 6000);
+      setVisible(false);
+      setConfirmLoading(false);
+    }, 2000);
   };
 
-  render() {
-    const { isLoading } = this.state;
-    return (
-      <>
+  const handleCancel = () => {
+    console.log('Clicked cancel button');
+    setVisible(false);
+  };
+
+  return (
+    <>
       <Button
         type="primary"
+        shape="round"
         icon={<ToTopOutlined />}
-        loading={isLoading}
-        onClick={() => this.setLoading()}
+        onClick={showModal}
       >
         Exfil Now
       </Button>
-      </>
-    );
-  }
-}
+      <Modal
+        title="Title"
+        visible={visible}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+      >
+        <p>{modalText}</p>
+      </Modal>
+    </>
+  );
+};
 
 ReactDOM.render(<App />, WidgetDiv);
